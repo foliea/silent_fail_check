4e78bca5357bb2e438d40ab3b25e3802e598d029
Adrien Folie <folie.adrien@gmail.com>
2014-04-07 06:06:31 +0200

    added eigenclass

--- lib/silent_fail_logger.rb
+++ lib/silent_fail_logger.rb
@@ -3,15 +3,17 @@ module SilentFailCheck
     @@options = {}
     @@is_configured = false
 
-    def self.configure opts = {}
-      @@options[:model] = opts[:model] || SilentFailLog
-      @@options[:message] = opts[:message] || 'message'
-      @@is_configured = true
-    end
+    class << self
+      def configure opts = {}
+        @@options[:model] = opts[:model] || SilentFailLog
+        @@options[:message] = opts[:message] || 'message'
+        @@is_configured = true
+      end
 
-    def self.add message
-      self.configure if !@is_configured
-      @@options[:model].send(:create, @@options[:message].to_sym => message)
+      def add message
+        self.configure if !@is_configured
+        @@options[:model].send(:create, @@options[:message].to_sym => message)
+      end
     end
   end
 end
