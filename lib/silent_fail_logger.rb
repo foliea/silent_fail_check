e8137379156ece69a7224d8e59d90b20b54a84da
Adrien Folie <folie.adrien@gmail.com>
2014-04-13 04:50:10 +0200

    extend self

--- lib/silent_fail_logger.rb
+++ lib/silent_fail_logger.rb
@@ -1,19 +1,19 @@
 module SilentFailCheck
-  class Logger
+  module Logger
+    extend self
+
     @@options = {}
     @@is_configured = false
 
-    class << self
-      def configure opts = {}
-        @@options[:model] = opts[:model] || SilentFailLog
-        @@options[:message] = opts[:message] || 'message'
-        @@is_configured = true
-      end
+    def configure opts = {}
+      @@options[:model] = opts[:model] || SilentFailLog
+      @@options[:message] = opts[:message] || 'message'
+      @@is_configured = true
+    end
 
-      def add message
-        self.configure if !@@is_configured
-        @@options[:model].send(:create, @@options[:message].to_sym => message)
-      end
+    def add message
+      self.configure if !@@is_configured
+      @@options[:model].send(:create, @@options[:message].to_sym => message)
     end
   end
 end
