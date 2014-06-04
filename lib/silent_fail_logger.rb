9e81ff15318dc7376de2ad185e015cca3bbaaab2
Adrien Folie <folie.adrien@gmail.com>
2014-06-04 03:53:43 +0200

    removed class singleton variable

--- lib/silent_fail_logger.rb
+++ lib/silent_fail_logger.rb
@@ -2,18 +2,18 @@ module SilentFailCheck
   module Logger
     extend self
 
-    @@options = {}
-    @@is_configured = false
+    @options = {}
+    @is_configured = false
 
     def configure(opts = {})
-      @@options[:model] = opts[:model] || SilentFailLog
-      @@options[:message] = opts[:message] || 'message'
-      @@is_configured = true
+      @options[:model] = opts[:model] || SilentFailLog
+      @options[:message] = opts[:message] || 'message'
+      @is_configured = true
     end
 
     def add(message)
-      configure unless @@is_configured
-      @@options[:model].send(:create, @@options[:message].to_sym => message)
+      configure unless @is_configured
+      @options[:model].send(:create, @options[:message].to_sym => message)
     end
   end
 end
