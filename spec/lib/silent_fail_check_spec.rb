ed1b66dd09a788259b77344e972ff776ffea8baf
Adrien Folie <folie.adrien@gmail.com>
2014-06-08 04:36:27 +0200

    spec new syntax

--- spec/lib/silent_fail_check_spec.rb
+++ spec/lib/silent_fail_check_spec.rb
@@ -5,8 +5,8 @@ describe SilentFailCheck do
   context 'when a failure happened' do
 
     it 'uses the logger' do
-      SilentFailCheck::Logger.should_receive(:add)
-                             .with("User : connection_time [\"is not a number\"]")
+      expect(SilentFailCheck::Logger).to receive(:add)
+        .with("User : connection_time [\"is not a number\"]")
       User.create(name: 'borris', connection_time: 'az')
     end
   end
