2ec3fe77e698cedcc6488dd43487b3bf0bcdf3ef
Adrien Folie <folie.adrien@gmail.com>
2014-10-28 11:36:42 +0100

    Update silent_fail_logger_spec.rb

--- spec/lib/silent_fail_logger_spec.rb
+++ spec/lib/silent_fail_logger_spec.rb
@@ -1,9 +1,7 @@
 require 'spec_helper'
 
 describe SilentFailCheck::Logger do
-
   context 'when logger is used' do
-
     it 'adds new log' do
       SilentFailCheck::Logger.add('Test')
       expect(SilentFailLog.count).to be > 0
