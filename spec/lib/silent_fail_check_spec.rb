3f1e653cff27d755124b6bfcedad4ce99f420bf8
Adrien Folie <folie.adrien@gmail.com>
2014-10-28 11:36:54 +0100

    Update silent_fail_check_spec.rb

--- spec/lib/silent_fail_check_spec.rb
+++ spec/lib/silent_fail_check_spec.rb
@@ -1,9 +1,7 @@
 require 'spec_helper'
 
 describe SilentFailCheck do
-
   context 'when a failure happened' do
-
     it 'uses the logger' do
       expect(SilentFailCheck::Logger).to receive(:add)
         .with("User : connection_time [\"is not a number\"]")
